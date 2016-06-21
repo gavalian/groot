acfc3a38c291e139f0b63e11c255b9f6c1f6f907
Will Phelps <willphelps@gmail.com>
2016-06-21 18:39:13 -0400

    UI Improvements
    -Changed default location of Panels
    -added a minimum size to CutPanel and DescriptorPanel
    -Changed Layout of options panels

--- src/main/java/org/jlab/groot/studio/StudioUI.java
+++ src/main/java/org/jlab/groot/studio/StudioUI.java
@@ -171,10 +171,12 @@ public class StudioUI implements MouseListener,ActionListener {
     public void addCut(){
         System.out.println("doing some stuff...");
         CutPanel cutPane = new CutPanel(studioTree);
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Cut Editor");
         //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(cutPane);
         frame.pack();
+        frame.setLocationRelativeTo(this.frame);
+        frame.setMinimumSize(frame.getSize());
         frame.setVisible(true);        
     }
     
@@ -227,9 +229,11 @@ public class StudioUI implements MouseListener,ActionListener {
     public void addDescriptor(){
         DescriptorPanel  panel = null;
         panel = new DescriptorPanel(studioTree,analyzer,2);
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Edit Histogram");
         frame.add(panel);
         frame.pack();
+        frame.setLocationRelativeTo(this.frame);
+        frame.setMinimumSize(frame.getSize());
         frame.setVisible(true);
     }
     
