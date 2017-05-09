7e8db4e6c45cda27d797220cca9d2ec417766b3f
Will Phelps <willphelps@gmail.com>
2017-05-09 19:13:26 -0400

    Added Modes to ParallelSliceFitter and inspectFits option

--- src/main/java/org/jlab/groot/data/H2F.java
+++ src/main/java/org/jlab/groot/data/H2F.java
@@ -366,6 +366,7 @@ public class H2F implements IDataSet {
         for(int loop = 0; loop < this.getXAxis().getNBins(); loop++){
             H1F slice = this.sliceX(loop);
             slice.setName(this.getName()+"_"+loop);
+            slice.setTitleY(this.getTitleY());
             slices.add(slice);
         }
         return slices;
@@ -376,6 +377,7 @@ public class H2F implements IDataSet {
         for(int loop = 0; loop < this.getYAxis().getNBins(); loop++){
             H1F slice = this.sliceY(loop);
             slice.setName(this.getName()+"_"+loop);
+            slice.setTitleX(this.getTitleX());
             slices.add(slice);
         }
         return slices;
--- src/main/java/org/jlab/groot/fitter/ParallelSliceFitter.java
+++ src/main/java/org/jlab/groot/fitter/ParallelSliceFitter.java
@@ -1,5 +1,7 @@
 package org.jlab.groot.fitter;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -9,6 +11,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 import org.jlab.groot.data.GraphErrors;
 import org.jlab.groot.data.H1F;
@@ -25,6 +28,23 @@ public class ParallelSliceFitter {
 	ArrayList<H1F> slices = null;
 	ArrayList<FitThread> threads = new ArrayList<FitThread>();
 	F1D fitsResults[];
+	double min = 0.0; 
+	double max = 0.0;
+	boolean autorange = true;
+	Axis axis = null;
+	String[] modes = {
+			"[amp]*gaus(x,[mean],[sigma])",
+			"[amp]*gaus(x,[mean],[sigma])+[p0]",
+			"[amp]*gaus(x,[mean],[sigma])+[p0]+x*[p1]",
+			"[amp]*gaus(x,[mean],[sigma])+[p0]+x*[p1]+x*x*[p2]",
+			"[amp]*gaus(x,[mean],[sigma])+[p0]+x*[p1]+x*x*[p2]+x*x*x[p3]",
+	};
+	public static final int NO_BG = -1;
+	public static final int P0_BG = 0;
+	public static final int P1_BG = 1;
+	public static final int P2_BG = 2;
+	public static final int P3_BG = 3;
+	int mode = 0;
 	
 	public H2F getHistogram() {
 		return histogram;
@@ -41,8 +61,10 @@ public class ParallelSliceFitter {
 	public void setNthreads(int nthreads) {
 		this.nthreads = nthreads;
 	}
-
-	Axis axis = null;
+	
+	public void setBackgroundOrder(int mode){
+		this.mode = mode+1;
+	}
 
 	public ParallelSliceFitter() {
 	}
@@ -54,21 +76,43 @@ public class ParallelSliceFitter {
 	public void fitSlicesY() {
 		slices = histogram.getSlicesY();
 		axis = histogram.getYAxis();
+		if(autorange && slices.size()>0){
+			min = slices.get(0).getxAxis().min();
+			max = slices.get(0).getxAxis().max();
+		}
 		fit();
 	}
 
 	public void fitSlicesX() {
 		slices = histogram.getSlicesX();
 		axis = histogram.getXAxis();
+		if(autorange && slices.size()>0){
+			min = slices.get(0).getxAxis().min();
+			max = slices.get(0).getxAxis().max();
+		}
 		fit();
+	}
+	
+	public GraphErrors getChi2Slices(){
+		GraphErrors graph = new GraphErrors();
+		//System.out.println("Slices size:"+slices.size());
+		for(int i=0; i<slices.size(); i++){
+			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getChiSquare()/(double)fitsResults[i].getNDF(),0,0);
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("#chi^2/ndf from SliceFitter");
+			//System.out.println("Bin:"+i+" X:"+axis.getBinCenter(i)+" Y:"+fitsResults[i].getParameter(1));
+		}
+		return graph;
 	}
 	
 	public GraphErrors getMeanSlices(){
 		GraphErrors graph = new GraphErrors();
-		System.out.println("Slices size:"+slices.size());
+		//System.out.println("Slices size:"+slices.size());
 		for(int i=0; i<slices.size(); i++){
 			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(1),0,fitsResults[i].parameter(1).error());
-			System.out.println("Bin:"+i+" X:"+axis.getBinCenter(i)+" Y:"+fitsResults[i].getParameter(1));
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Gaussian Mean from SliceFitter");
+			//System.out.println("Bin:"+i+" X:"+axis.getBinCenter(i)+" Y:"+fitsResults[i].getParameter(1));
 		}
 		return graph;
 	}
@@ -77,6 +121,8 @@ public class ParallelSliceFitter {
 		GraphErrors graph = new GraphErrors();
 		for(int i=0; i<slices.size(); i++){
 			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(2),0,fitsResults[i].parameter(2).error());
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Gaussian #sigma from SliceFitter");
 		}
 		return graph;
 	}
@@ -85,8 +131,135 @@ public class ParallelSliceFitter {
 		GraphErrors graph = new GraphErrors();
 		for(int i=0; i<slices.size(); i++){
 			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(0),0,fitsResults[i].parameter(0).error());
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Gaussian Amplitude from SliceFitter");
 		}
 		return graph;
+	}
+	
+	public GraphErrors getP0Slices(){
+		GraphErrors graph = new GraphErrors();
+		for(int i=0; i<slices.size(); i++){
+			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(3),0,fitsResults[i].parameter(3).error());
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Background P0 from SliceFitter");
+		}
+		return graph;
+	}
+	
+	public GraphErrors getP1Slices(){
+		GraphErrors graph = new GraphErrors();
+		for(int i=0; i<slices.size(); i++){
+			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(4),0,fitsResults[i].parameter(4).error());
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Background P1 from SliceFitter");
+		}
+		return graph;
+	}
+	
+	public GraphErrors getP2Slices(){
+		GraphErrors graph = new GraphErrors();
+		for(int i=0; i<slices.size(); i++){
+			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(5),0,fitsResults[i].parameter(5).error());
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Background P2 from SliceFitter");
+		}
+		return graph;
+	}
+	
+	public GraphErrors getP3Slices(){
+		GraphErrors graph = new GraphErrors();
+		for(int i=0; i<slices.size(); i++){
+			graph.addPoint(axis.getBinCenter(i),fitsResults[i].getParameter(6),0,fitsResults[i].parameter(6).error());
+			graph.setTitleX(this.slices.get(0).getTitleX());
+			graph.setTitleY("Background P3 from SliceFitter");
+		}
+		return graph;
+	}
+
+
+	public void setMin(double min) {
+		this.min = min;
+		this.autorange = false;
+	}
+
+	public void setMax(double max) {
+		this.max = max;
+		this.autorange = false;
+	}
+	
+	public void setRange(double min,double max) {
+		this.min = min;
+		this.max = max;
+		this.autorange = false;
+	}
+	
+	public void inspectFits(){
+		int nHistograms = slices.size();
+		int cols = 4;
+		int rows = 4;
+		//int pages = nHistograms/(cols*rows) + ((nHistograms%(cols*rows)==0)?0:1);
+		int pages = nHistograms/(cols*rows);
+		if(nHistograms - pages*cols*rows > 0) pages++;
+
+		JFrame frame = new JFrame("Fit Inspector");
+		JTabbedPane pane = new JTabbedPane();
+		EmbeddedCanvas fitSummary = new EmbeddedCanvas();
+		int nPlots = mode+5;
+		fitSummary.divide(3, 2);
+		if(nPlots>6){
+			fitSummary.divide(3, 3);
+		}else if(nPlots>9){
+			fitSummary.divide(4, 3);
+		}else if(nPlots>12){
+			fitSummary.divide(4, 4);
+		}
+		int counter = 0;
+		fitSummary.cd(counter++);
+		fitSummary.draw(this.histogram);
+		fitSummary.cd(counter++);
+		fitSummary.draw(this.getMeanSlices());
+		fitSummary.cd(counter++);
+		fitSummary.draw(this.getSigmaSlices());
+		fitSummary.cd(counter++);
+		fitSummary.draw(this.getAmpSlices());
+		if(mode>0){
+			fitSummary.cd(counter++);
+			fitSummary.draw(this.getP0Slices());
+			if(mode>1){
+				fitSummary.cd(counter++);
+				fitSummary.draw(this.getP1Slices());
+			}
+			if(mode>2){
+				fitSummary.cd(counter++);
+				fitSummary.draw(this.getP2Slices());
+			}
+			if(mode>3){
+				fitSummary.cd(counter++);
+				fitSummary.draw(this.getP3Slices());
+			}
+		}
+		fitSummary.cd(counter++);
+		fitSummary.draw(this.getChi2Slices());
+		pane.addTab("Summary",fitSummary);
+		
+		for(int i=0; i<pages; i++){
+			EmbeddedCanvas temp = new EmbeddedCanvas();
+			temp.divide(cols, rows);
+			for(int j=0; j<(rows*cols)&&(j+i*rows*cols)<nHistograms; j++){
+				temp.cd(j);
+				temp.draw(slices.get(i*rows*cols+j));
+				//temp.draw(fitsResults[i],"same");
+				temp.getPad(j).setTitle("Slice #"+((i*rows*cols)+j)+" Value:"+String.format("%.2f", this.axis.getBinCenter(i*rows*cols+j)));
+			}
+			pane.add("Page "+i, temp);
+		}
+		frame.add(pane);
+		Dimension screensize  = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.pack();
+		frame.setSize((int)(screensize.getHeight()*.75*1.618), (int) (screensize.getHeight()*.75));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 	private void fit() {
@@ -104,7 +277,7 @@ public class ParallelSliceFitter {
 			temp.setN(i);
 			temp.setHistogram(slice);
 			temp.setResultSetter(setter);
-			temp.setFitter(new F1D("f1", "[amp]*gaus(x,[mean],[sigma])", slice.getxAxis().min(), slice.getxAxis().max()));
+			temp.setFitter(new F1D("f1",modes[mode], min, max));
 			threads.add(temp);
 		}
 
