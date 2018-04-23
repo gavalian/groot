cfc3a6a2006a14227fcd215334b7d521167a69a6
gavalian <gavalian@meson.jlab.org>
2018-04-23 10:18:33 -0400

    Fixed the issue with TDirectory not reading all the entries in the file

--- src/main/java/org/jlab/groot/data/TDirectory.java
+++ src/main/java/org/jlab/groot/data/TDirectory.java
@@ -152,13 +152,20 @@ public class TDirectory extends Directory<IDataSet> {
         System.out.println(" DEBUG : event count = " + nevents);
         int counter = 0;
         int groupCounter = 0;
+        
+        int nrecords = reader.getRecordCount();
+        
         //for(int i = 0; i < nevents; i++){
-        while(reader.hasNext()==true){
+        //while(reader.hasNext()==true){
             //byte[] eventBuffer = reader.readEvent(i);
             //System.out.println(" EVENT # " + i + "  SIZE = " + eventBuffer.length);
-            HipoEvent    event = reader.readNextEvent();
-            
-            if(event.hasGroup(1200)==true){
+        for(int r = 0 ; r < nrecords; r++){
+            boolean status = reader.readRecord(r);
+            int nrevents = reader.getRecordEventCount();
+            for(int e = 0; e < nrevents; e++){
+                //HipoEvent    event = reader.readNextEvent();
+                HipoEvent    event = reader.readRecordEvent(e);
+                if(event.hasGroup(1200)==true){
                 groupCounter++;
                 System.out.println("--> reading data group descriptor");
                 System.out.println(event);
@@ -175,14 +182,16 @@ public class TDirectory extends Directory<IDataSet> {
                 //System.out.println("[DIRNAME] = [" + dirname + "]");
                 mkdir(dirname);
                 cd(dirname);
-                pwd();
+                //pwd();
                 //System.out.println("OBJECTS = " + this.getObjectList().size());
                 h1.setName(this.stringObjectFromPath(h1name));
                 //System.out.println(h1.toString());
                 addDataSet(h1);
                 //this.ls();
+                }
+                counter++;
             }
-            counter++;
+            
         }
         System.out.println(" EVENT # " + nevents + " : counter = " +
                 counter + " groups = " + groupCounter);
@@ -203,6 +212,8 @@ public class TDirectory extends Directory<IDataSet> {
         
         if(args.length<3){
             System.out.println("error: \n\n Usage : hadd [outputfile] [input1] [input2] ....\n\n");
+            String filename = "ftCalCosmic_0_04-04-2018_05.59.18_PM.hipo";
+            dir.readFile(filename);
             System.exit(0);
         }
         
