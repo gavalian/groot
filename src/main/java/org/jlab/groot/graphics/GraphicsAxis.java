e83b0c34457337e58e2e77f5d995813595ce9f63
andru quiroga <andru.quiroga.18@cnu.edu>
2021-02-23 16:27:28 -0500

    more PDF bug fixes;

--- src/main/java/org/jlab/groot/graphics/GraphicsAxis.java
+++ src/main/java/org/jlab/groot/graphics/GraphicsAxis.java
@@ -395,9 +395,10 @@ public class GraphicsAxis {
             }
             
             int axisBounds = (int) texts.get(0).getBoundsNumber(g2d).getHeight();
-            attr.getTitle().drawString(g2d,
-                    (int) midpoint,
-                    y + axisBounds + labelOffset + titleOffset, 1, 0);
+            if (!attr.getTitle().getTextString().equals(""))
+                attr.getTitle().drawString(g2d,
+                        (int) midpoint,
+                        y + axisBounds + labelOffset + titleOffset, 1, 0);
         } else {
             g2d.drawLine(x, (int) this.attr.getAxisDimension().getMin(), x, 
                     (int) this.attr.getAxisDimension().getMax());
